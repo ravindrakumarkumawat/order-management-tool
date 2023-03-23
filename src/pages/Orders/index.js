b15e77caa28367170deae13fea6f8a6047470e2a
Ravindra Kumawat <ravindra.kumawat7676@gmail.com>
2023-03-23 09:11:59 +0000

    Added the sorting

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,29 +1,70 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Pagination from '../../component/common/Pagination';
 import data from '../../mock/orderData.json';
 import './index.css';
-
+const ASC = 'asc'
+const DESC = 'desc'
 const Orders = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
     const ITEMS_PER_PAGE = 10;
 
-    const filteredData = data.filter((item) =>
-        Object.values(item).some((value) => 
-            value.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-    );
+    const [vendorData, setVendorData] = useState([...data]);
+    useEffect(() => {
+        setVendorData(data.filter((item) =>
+            Object.values(item).some((value) => 
+                value.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+        ))
+    }, [searchTerm])
+    
     const getCurrentItems = () => {
         const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
         const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
-        return filteredData.slice(indexOfFirstItem, indexOfLastItem);
+        
+        return vendorData.slice(indexOfFirstItem, indexOfLastItem);
     };
     const getPageCount = () => {
-        return Math.ceil(filteredData.length / ITEMS_PER_PAGE);
+        return Math.ceil(vendorData.length / ITEMS_PER_PAGE);
     };
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
+    const [sortOrder, setOrder] = useState({
+        vendorName: ASC,
+        pickupDate: ASC,
+        status: ASC,
+    })
+    useEffect(() => {
+        setVendorData([...sortBy('vendorName')])
+        
+    }, [sortOrder.vendorName])
+    useEffect(() => {
+        setVendorData([...sortBy('pickupDate')])
+        
+    }, [sortOrder.pickupDate])
+    useEffect(() => {
+        setVendorData([...sortBy('status')])
+        
+    }, [sortOrder.status])
+
+    const toggleSortedOrder = (type) => {
+        setOrder({
+            ...sortOrder,
+            [type]: sortOrder[type] === ASC ? DESC : ASC
+        })
+    }
+    
+    const sortBy = (key) => {
+        return vendorData.sort((a, b) => {
+            if(sortOrder[key] == ASC) {
+                return a[key].localeCompare(b[key])
+            } else if(sortOrder[key] == DESC) {
+                return b[key].localeCompare(a[key])
+            }
+        })
+    }
+    
     return (
         <div className="orders-page">
             <h1>Orders Page</h1>
@@ -39,9 +80,9 @@ const Orders = () => {
                     <thead>
                     <tr>
                         <th>Order Id</th>
-                        <th>Vendor name</th>
-                        <th>Pick up date</th>
-                        <th>Status</th>
+                        <th onClick={() => toggleSortedOrder('vendorName')}>Vendor name {sortOrder['vendorName']}</th>
+                        <th onClick={() => toggleSortedOrder('pickupDate')}>Pick up date {sortOrder['pickupDate']}</th>
+                        <th onClick={() => toggleSortedOrder('status')}>Status {sortOrder['status']}</th>
                     </tr>
                     </thead>
                     <tbody>
